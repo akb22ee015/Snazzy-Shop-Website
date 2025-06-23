3230b17ef502ba38de12609d6fc708a9a6547bbf
Atish Kumar <b22ee015@iitj.ac.in>
2025-06-23 08:15:14 +0530

    Create Admin dashboard

---
+++ admin/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL; // ✅ Make sure this line is correct
+
+const App = () => {
+  const [token, setToken] = useState("");
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      {token === "" ? (
+        <Login setToken={setToken} /> // ✅ Pass setToken to Login
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="w-full flex">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
