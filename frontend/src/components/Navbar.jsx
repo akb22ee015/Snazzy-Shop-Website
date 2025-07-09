f66d539fd225a9a7aee3b94baa06502b5ce8ff97
Atish Kumar <b22ee015@iitj.ac.in>
2025-07-09 20:29:44 +0530

    Fix Login and SignUp Page with backend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,22 @@ import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const {setShowSearch,getCartCount} = useContext(ShopContext);
+  const {
+    setShowSearch,
+    getCartCount,
+    token,
+    navigate,
+    setToken,
+    setCartItems,
+  } = useContext(ShopContext);
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    setCartItems({});
+    navigate("/login");
+  };
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
@@ -39,18 +54,25 @@ const Navbar = () => {
           alt=""
         />
         <div className="group relative">
-          <Link to={'/login'}><img
-            src={assets.profile_icon}
-            className="w-5 cursor-pointer"
-            alt=""
-          /></Link>
-          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 rounded text-gray-500">
-              <p className="cursor-pointer hover:text-black">My Profile</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
-              <p className="cursor-pointer hover:text-black">Log Out</p>
+          {/* <Link to={"/login"}> */}
+            <img
+              onClick={() => token ? null :navigate('/login')}
+              src={assets.profile_icon}
+              className="w-5 cursor-pointer"
+              alt=""
+            />
+          {/* </Link> */}
+          {token && (
+            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+              <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 rounded text-gray-500">
+                <p className="cursor-pointer hover:text-black">My Profile</p>
+                <p onClick={()=>navigate('/orders')} className="cursor-pointer hover:text-black">Orders</p>
+                <p onClick={logout} className="cursor-pointer hover:text-black">
+                  Log Out
+                </p>
+              </div>
             </div>
-          </div>
+          )}
         </div>
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
