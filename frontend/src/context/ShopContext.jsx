0f07decf757de15b25d14f6138c7a59a6fd4c74d
Atish Kumar <b22ee015@iitj.ac.in>
2025-07-20 20:24:34 +0530

    Cart functionality connect with database

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -70,6 +70,16 @@ const ShopContextProvider = (props) => {
     cartData[itemId][size] = quantity;
 
     setCartItems(cartData);
+
+    if(token){
+      try {
+        
+        await axios.post(backendUrl + '/api/cart/update',{itemId, size, quantity},{headers:{token}})
+      } catch (error) {
+        console.log(error.message)
+        toast.error(error.message)
+      }
+    }
   };
 
   const getCartAmount = ()=>{
@@ -105,10 +115,23 @@ const ShopContextProvider = (props) => {
       console.log(error)
       toast.error(error.message)
     }
+  }
 
-  
+  const getUserCart = async (token) => {
 
-    
+    try {
+      
+      const response = await axios.post(backendUrl + '/api/cart/get' , {} , {headers:{token}})
+
+      if(response.data.success){
+        setCartItems(response.data.cartData)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+
   }
 
   useEffect(()=>{
@@ -120,6 +143,7 @@ const ShopContextProvider = (props) => {
     useEffect(()=>{
       if(!token && localStorage.getItem('token')){
         setToken(localStorage.getItem('token'));
+        getUserCart(localStorage.getItem('token'))
       }
     },[])
 
