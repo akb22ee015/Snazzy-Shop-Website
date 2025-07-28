254a6b03fd2885392df73bec30befe079a9d729a
Atish Kumar <b22ee015@iitj.ac.in>
2025-07-28 22:15:48 +0530

    Place Order feature

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,38 @@
+
+
+//Placing order using COD Method
+
+const placeOrder = async (req,res) => {
+
+}
+
+//using Stripe
+
+const placeOrderStripe = async (req,res) => {
+    
+}
+
+//using rozerpay
+
+const placeOrderRozerpay = async (req,res) => {
+    
+}
+
+//All oders details at admin
+const allOrders = async (req,res) => {
+    
+}
+
+//user data at frontend
+
+const userOrders = async (req,res) => {
+    
+}
+
+//update order status 
+
+const updateStatus = async (req,res) => {
+    
+}
+
+export {placeOrder,placeOrderStripe, placeOrderRozerpay, userOrders, allOrders, updateStatus}
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -20,11 +20,11 @@ const Contact = () => {
           <p className="font-semibold text-xl text-gray-600">Our Store</p>
           <p className="text-gray-500">
             54709 Willms Station <br />
-            Suite 350, Washington, USA
+            Kawa 350, Mumbai, IN
           </p>
           <p className="text-gray-500">
             Tel: (415) 555-0132 <br />
-            Email: admin@forever.com
+            Email: admin@iitian.com
           </p>
           <p className="font-semibold text-xl text-gray-600">
             Careers at Forever
